7ee5130b34328830c72405bb92f21df569432649
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2021-12-29 21:02:12 -0500

    Time: 26 ms (47.68%), Space: 9.9 MB (58.50%) - LeetHub

--- combinations/combinations.cpp
+++ combinations/combinations.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> res;
+    int n;
+    int k;
+    vector<int> vec;
     
-    void back_track(int n, int k, vector<int> vec, int start) {
+    
+    void back_track(int start) {
         if(vec.size() == k) {
             res.push_back(vec);
             return;
@@ -10,14 +14,15 @@ public:
         
         for(int i=start; i<=n; i++) {
             vec.push_back(i);
-            back_track(n, k, vec, i+1);
+            back_track(i+1);
             vec.pop_back();
         }
     }
     
-    vector<vector<int>> combine(int n, int k) {
-        vector<int> vec;
-        back_track(n, k, vec, 1);
+    vector<vector<int>> combine(int p, int q) {
+        n = p;
+        k = q;
+        back_track(1);
         return res;
     }
 };
