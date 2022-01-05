5095beb16610302c89ddf80558374cfaced0f762
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-01-05 00:13:35 -0500

    Time: 509 ms (12.41%), Space: 48.7 MB (17.50%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,9 +3,12 @@ public:
     bool canJump(vector<int>& nums) {
         vector<bool> memo(nums.size(), false);
         memo[nums.size()-1] = true;
+        int b = nums.size()-1;
         
         for(int i=nums.size()-2; i>=0; i--) {
-            for(int j=i+1; j<=i+nums[i]; j++) {
+            int a = i+nums[i];
+            int mn = min(a,b);
+            for(int j=i+1; j<=mn; j++) {
                 if(memo[j]) {
                     memo[i]=true;
                     break;
