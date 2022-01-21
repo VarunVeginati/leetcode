461662143334f43e201bd0ed6244e329882688c8
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-01-21 13:24:36 -0500

    Time: 397 ms (5.00%), Space: 49.8 MB (83.67%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int next_station;
-    vector<int> gas;
     
-    bool can_travel_back(int i, vector<int>& cost) {
+    bool can_travel_back(int i, vector<int>& gas, vector<int>& cost) {
         int current_index = i;
         if(gas[i] < cost[i]) {
             next_station = i%gas.size();
@@ -26,11 +25,10 @@ public:
         return true;
     }
     
-    int canCompleteCircuit(vector<int>& gass, vector<int>& cost) {
-        gas = gass;
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
         for(int i=0; i<gas.size(); i++) {
-            if(can_travel_back(i, cost)) return i;
+            if(can_travel_back(i, gas, cost)) return i;
             else {
                 if(next_station > i)
                     i = next_station;
