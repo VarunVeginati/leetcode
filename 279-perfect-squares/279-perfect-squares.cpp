2255689d73748a01be52b4ff50cb2153acffc6fa
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-01-22 13:28:07 -0500

    Time: 411 ms (17.69%), Space: 9.1 MB (62.00%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    vector<int> memo;
-    
-    int dp(int n) {
-        if(n==1) return 1;
-        if(n<1) return 0;
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[1]=1;
+        dp[0]=0;
         
-        if(memo[n] != 0) return memo[n];
-        
-        int mn = INT_MAX;
-        
-        for(int i=1; i*i<=n; i++) {
-            mn = min(mn, 1+dp(n-(i*i)));
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j*j<=i; j++) {
+                dp[i] = min(dp[i], 1+dp[i-(j*j)]);
+            }
         }
         
-        memo[n] = mn;
-        
-        return mn;
-    }
-    
-    int numSquares(int n) {
-        memo = vector<int>(n+1, 0);
-        
-        return dp(n);
+        return dp[n];
     }
 };
