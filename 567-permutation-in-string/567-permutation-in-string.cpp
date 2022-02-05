94411613e593b3a4f288b70a95dc01c6f4950104
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-02-05 00:11:42 -0500

    Time: 12 ms (51.93%), Space: 7.6 MB (23.15%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,12 +2,28 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if(s1.length() > s2.length()) return false;
-        sort(s1.begin(), s1.end());
+        
+        map<char,int> s1m;
+        map<char,int> s2m;
+        
+        for(char i='a'; i<='z'; i++) {
+            s1m[i] = 0;
+            s2m[i] = 0;
+        }
+        
+        for(int i=0; i<s1.length(); i++)
+            s1m[s1[i]]++;
+        
+        for(int i=0; i<s1.length(); i++)
+            s2m[s2[i]]++;
+        
+        if(s1m==s2m) return true;
         
         for(int i=0; i<s2.length()-s1.length()+1; i++) {
-            string s = s2.substr(i, s1.length());
-            sort(s.begin(), s.end());
-            if(s==s1) return true;
+            s2m[s2[i]]--;
+            s2m[s2[i+s1.length()]]++;
+            
+            if(s1m==s2m) return true;
         }
         
         return false;
