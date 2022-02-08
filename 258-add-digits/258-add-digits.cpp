8fcded114a924953f3c9a1044287ad8bb2aace86
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-02-07 21:06:27 -0500

    Time: 4 ms (43.34%), Space: 5.9 MB (75.31%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9) {
+            int a = 0;
+            while(num>0){
+                a += num%10;
+                num=num/10;
+            }
+            num = a;
+        }
+        
+        return num;
+    }
+};
