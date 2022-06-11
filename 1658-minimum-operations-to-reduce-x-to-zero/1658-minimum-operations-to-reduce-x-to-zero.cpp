1621d893755cf05e903b4a4748cf231fa757daac
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-06-11 11:07:42 -0400

    Time: 322 ms (49.69%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,64 +1,37 @@
 class Solution {
-    
 public:
     int minOperations(vector<int>& nums, int x) {
-        vector<int> vec1 = {nums[0]};
-        vector<int> vec2 = {nums.back()};
-        map<int, int> m1;
-        map<int, int> m2;
-        int n = nums.size();
-        
-        m1[nums[0]] = 0;
-        m2[nums.back()] = 0;
-        
-        for(int i=1; i<n; i++) {
-            vec1.push_back(nums[i]+vec1[i-1]);
-            m1[vec1[i]]=i;
-            vec2.push_back(nums[n-i-1]+vec2[i-1]);
-            m2[vec2[i]]=i;
+        int total = 0;
+        for(int i=0; i<nums.size(); i++) {
+            total += nums[i];
         }
         
-        if(x>vec1.back() && x>vec2.back()) return -1;
+        int sum = total-x;
         
-        int mn = INT_MAX;
+        if(sum<0) return -1;
         
-        for(int i=0; i<n; i++) {
-            int target1 = x-vec1[i];
-            int target2 = x-vec2[i];
-            if(target1<0 && target2<0) break;
-            if(m2.find(target1) != m2.end()) {
-                mn = min(mn, i+m2[target1]+2);
-            }
-            if(m1.find(target2) != m1.end()) {
-                mn = min(mn, i+m1[target2]+2);
-            }
-            else if(target1==0 || target2==0) {
-                mn = min(mn, i+1);
+        int left = 0;
+        int right = 0;
+        
+        int sub_array_sum = nums[0];
+        
+        int min_operations = INT_MAX;
+        
+        while(right<nums.size()) {
+            if(sub_array_sum == sum) {
+                int a = nums.size() - (right-left+1);
+                min_operations = min(min_operations, a);
+                right++;
+                if(right<nums.size()) sub_array_sum += nums[right];
+            } else if(sub_array_sum<sum) {
+                right++;
+                if(right<nums.size()) sub_array_sum += nums[right];
+            } else {
+                sub_array_sum -= nums[left];
+                left++;
             }
         }
         
-//         for(int i=0; i<n; i++) {
-//             int target = x-vec1[i];
-//             if(target<0) break;
-//             if(m2.find(target) != m2.end()) {
-//                 mn = min(mn, i+m2[target]+2);
-//             }
-//             else if(target==0) {
-//                 mn = min(mn, i+1);
-//             }
-//         }
-        
-//         for(int i=0; i<n; i++) {
-//             int target = x-vec2[i];
-//             if(target<0) break;
-//             if(m1.find(target) != m1.end()) {
-//                 mn = min(mn, i+m1[target]+2);
-//             }
-//             else if(target==0) {
-//                 mn = min(mn, i+1);
-//             }
-//         }
-        
-        return mn==INT_MAX ? -1 : mn;
+        return min_operations == INT_MAX ? -1: min_operations;
     }
 };
