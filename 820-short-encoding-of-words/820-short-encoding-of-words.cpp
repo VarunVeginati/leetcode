80293b864f3ff4c5af5d1de83a99175c94f1fd77
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-06-20 22:33:00 -0400

    Time: 136 ms (43.63%), Space: 18.2 MB (69.61%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        sort(words.begin(), words.end(), [](const string &a, const string &b)->bool{
-           return a.length()>b.length(); 
-        });
+        // sort(words.begin(), words.end(), [](const string &a, const string &b)->bool{
+        //    return a.length()>b.length(); 
+        // });
         
         set<string> s;
         
@@ -11,17 +11,16 @@ public:
             s.insert(words[i]);
         }
         
-        int sum = 0;
-        
-        for(int i=0; i<words.size(); i++) {
-            string str = words[i];
-            if(s.find(str) == s.end()) continue;
-            
-            for(int j=0; j<str.length(); j++) {
+        for(string str: words) {
+            for(int j=1; j<str.length(); j++) {
                 if(s.find(str.substr(j)) != s.end())
                     s.erase(str.substr(j));
             }
-            
+        }
+        
+        int sum = 0;
+        
+        for(string str : s) {
             sum += str.length()+1;
         }
         
