eaac3a751f93fbf119a426c458b7800d7ec633b9
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-07-10 22:15:36 -0400

    Time: 28 ms (57.63%), Space: 11.5 MB (42.07%) - LeetHub

--- 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.cpp
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.cpp
@@ -11,43 +11,21 @@ public:
 
 class Solution {
 public:
-    Node* getParent(Node* node) {
-        while(node->parent!=NULL)
-            node = node->parent;
-        
-        return node;
-    }
-    
-    void getInorder(Node* root, vector<int> &vec) {
-        if(root==NULL) return;
-        
-        getInorder(root->left, vec);
-        vec.push_back(root->val);
-        getInorder(root->right, vec);
-    }
-    
     Node* inorderSuccessor(Node* node) {
-        Node* root = getParent(node);
-        
-        vector<int> vec;
-        getInorder(root, vec);
-        
-        int resVal = INT_MIN;
-        
-        for(int i=0; i<vec.size()-1; i++) {
-            if(node->val == vec[i])
-                resVal = vec[i+1];
+        if(node->right != NULL) {
+            node=node->right;
+            while(node->left!=NULL)
+                node=node->left;
+            return node;
         }
         
-        if(resVal==INT_MIN) return NULL;
-        
-        while(root->val != resVal) {
-            if(root->val > resVal)
-                root = root->left;
+        while(node->parent!=NULL) {
+            if(node->parent->left == node)
+                return node->parent;
             else
-                root = root->right;
+                node=node->parent;
         }
         
-        return root;
+        return NULL;
     }
 };
