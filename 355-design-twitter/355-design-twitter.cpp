cc02551ed825cf76fcd6d0100ee4550b0c5aacff
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-07-17 11:50:57 -0400

    Time: 3 ms (58.79%), Space: 7.6 MB (11.92%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -3,7 +3,7 @@ private:
     map<int, set<int> > followers;
     map<int, set<int> > following;
     map<int, vector<pair<int,int> >> tweets;
-    priority_queue<vector<int>> feed;
+    map<int, priority_queue<vector<int>>> feed;
     int time;
     
 public:
@@ -12,17 +12,21 @@ public:
     }
     
     void postTweet(int userId, int tweetId) {
-        feed.push({time, tweetId, userId});
+        tweets[userId].push_back(make_pair(tweetId, time));
+        feed[userId].push({time, tweetId, userId});
+        for(auto follower: followers[userId]) {
+            feed[follower].push({time, tweetId, userId});
+        }
         time++;
     }
     
     vector<int> getNewsFeed(int userId) {
-        priority_queue<vector<int>> pq = feed;
+        priority_queue<vector<int>> pq = feed[userId];
         vector<int> res;
         set<int> followingUsers = following[userId];
         
         while(!pq.empty() && res.size()<10) {
-            if((pq.top()[2]==userId || followingUsers.find(pq.top()[2]) != followingUsers.end()))
+            if(pq.top()[2]==userId || followingUsers.find(pq.top()[2]) != followingUsers.end())
                 res.push_back(pq.top()[1]);
             pq.pop();
         }
@@ -31,11 +35,20 @@ public:
     }
     
     void follow(int followerId, int followeeId) {
+        if(following[followerId].find(followeeId) != following[followerId].end()) return;
+        followers[followeeId].insert(followerId);
         following[followerId].insert(followeeId);
+        
+        vector<pair<int,int> > followeeTweets = tweets[followeeId];
+        
+        for(pair<int,int> tweet: followeeTweets) {
+            feed[followerId].push({tweet.second, tweet.first, followeeId});
+        }
     }
     
     void unfollow(int followerId, int followeeId) {
         following[followerId].erase(followeeId);
+        followers[followeeId].erase(followerId);
     }
 };
 
