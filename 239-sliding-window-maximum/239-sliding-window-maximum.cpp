2bf51b41496a989bba3d77933fa00ed047dc85b3
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-07-26 22:46:20 -0400

    Time: 416 ms (64.84%), Space: 136.9 MB (37.46%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int> > pq;
+        deque<pair<int,int> > dq;
+        
+        for(int i=0; i<k; i++) {
+            while(!dq.empty() && dq.back().first<nums[i])
+                dq.pop_back();
+            
+            dq.push_back(make_pair(nums[i], i));
+        }
+        
         vector<int> res;
         
-        for(int i=0; i<k; i++)
-            pq.push(make_pair(nums[i], i));
+        res.push_back(dq.front().first);
         
-        res.push_back(pq.top().first);
+        int left = 1;
         
-        for(int i=1; i<=nums.size()-k; i++) {
-            pq.push(make_pair(nums[i+k-1], i+k-1));
+        for(int i=k; i<nums.size(); i++) {
+            while(!dq.empty() && dq.front().second<left) dq.pop_front();
             
-            while(pq.top().second<i || pq.top().second>=i+k) {
-                pq.pop();
-            }
+            while(!dq.empty() && dq.back().first<nums[i]) dq.pop_back();
+
+            dq.push_back(make_pair(nums[i],i));
             
-            res.push_back(pq.top().first);
+            res.push_back(dq.front().first);
+            left++;
         }
         
         return res;
