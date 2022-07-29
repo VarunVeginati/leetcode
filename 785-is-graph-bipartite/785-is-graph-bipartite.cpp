99c5ec63f2624506fd95bf64fbd3a6553b43b461
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-07-28 22:31:37 -0400

    Time: 138 ms (5.04%), Space: 14.3 MB (18.83%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        queue<int> q;
         int n = graph.size();
-        vector<bool> visited(n, false);
-        vector<int> color(n, -1);
-        for(int i=0;i<n;i++)
-        {
-            if(!visited[i])
-            {
-                q.push(i);
-                color[i] = 1;
-                while(!q.empty())
-                {
-                    int s = q.front();
+        vector<int> vec(n, 0);
+        set<int> visited;
+        queue<int> q;
+        
+        for(int i=0; i<n; i++) {
+            if(visited.find(i)!=visited.end()) continue;
+            
+            q.push(i);
+            int a = 1;
+            vec[i] = a;
+            
+            while(!q.empty()) {
+                a=a*-1;
+                int size = q.size();
+                
+                for(int i=0; i<size; i++) {
+                    int curr = q.front();
                     q.pop();
-                    visited[s]=true;
-                    for(int j=0;j<graph[s].size();j++)
-                    {
-                        if(color[s]==color[graph[s][j]])
-                            return false;
-                        else if(color[graph[s][j]]==-1)
-                        {
-                            color[graph[s][j]] = 1-color[s];
-                            q.push(graph[s][j]);
-                        }
+                    
+                    // vector<int> neighbours = graph[curr];
+                    visited.insert(curr);
+                    
+                    for(int j=0; j<graph[curr].size(); j++) {
+                        if(visited.find(graph[curr][j]) != visited.end()) continue;
+                        
+                        if(vec[graph[curr][j]]==0) vec[graph[curr][j]] = a;
+                        else if(vec[graph[curr][j]] != a) return false;
+                        
+                        q.push(graph[curr][j]);
                     }
                 }
             }
         }
+        
         return true;
     }
 };
