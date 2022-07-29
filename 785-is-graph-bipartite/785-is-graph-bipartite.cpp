6df955d0f0b159c458d4c1902f2f3e7b5873ae9a
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-07-28 22:30:40 -0400

    Time: 33 ms (67.89%), Space: 13.6 MB (42.43%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vec(n, 0);
-        set<int> visited;
         queue<int> q;
-        
-        for(int i=0; i<n; i++) {
-            if(visited.find(i)!=visited.end()) continue;
-            
-            q.push(i);
-            int a = 1;
-            vec[i] = a;
-            
-            while(!q.empty()) {
-                a=a*-1;
-                int size = q.size();
-                
-                for(int i=0; i<size; i++) {
-                    int curr = q.front();
+        int n = graph.size();
+        vector<bool> visited(n, false);
+        vector<int> color(n, -1);
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                q.push(i);
+                color[i] = 1;
+                while(!q.empty())
+                {
+                    int s = q.front();
                     q.pop();
-                    
-                    vector<int> neighbours = graph[curr];
-                    visited.insert(curr);
-                    
-                    for(int j=0; j<neighbours.size(); j++) {
-                        if(visited.find(neighbours[j]) != visited.end()) continue;
-                        
-                        if(vec[neighbours[j]]==0) vec[neighbours[j]] = a;
-                        else if(vec[neighbours[j]] != a) return false;
-                        
-                        q.push(neighbours[j]);
+                    visited[s]=true;
+                    for(int j=0;j<graph[s].size();j++)
+                    {
+                        if(color[s]==color[graph[s][j]])
+                            return false;
+                        else if(color[graph[s][j]]==-1)
+                        {
+                            color[graph[s][j]] = 1-color[s];
+                            q.push(graph[s][j]);
+                        }
                     }
                 }
             }
         }
-        
         return true;
     }
 };
