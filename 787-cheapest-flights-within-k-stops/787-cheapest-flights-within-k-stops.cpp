315fb1578cb9336e4806e2f4520391499b5e4efc
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-08-26 19:02:28 -0400

    Time: 55 ms (52.10%), Space: 23.3 MB (23.85%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,48 +1,46 @@
-// since the question asked about with atmost k stops we can't use Dijkstras algorithm. Hence we need to go with Bellman Ford algorithm.
-
 class BellmanFord{
 private:
-	vector<vector<int>> dp;
-	vector<vector<vector<int>>> edges;
-
+    vector<vector<int>> dp;
+    vector<vector<pair<int,int> >> edges;
+    
 public:
-	BellmanFord(int k, int n, vector<vector<int>> graph) {
-		dp = vector<vector<int>>(k+1, vector<int>(n, INT_MAX));
-		edges = vector<vector<vector<int>>>(n);
-
-		for(int i=0; i<graph.size(); i++) {
-			edges[graph[i][1]].push_back({graph[i][0], graph[i][2]});
-		}
-	}
-
-	int findMinCost(int source, int destination, int k, int n) {
-		dp[0][source] = 0;
-
-		for(int i=1; i<=k; i++) {
-			for(int j=0; j<n; j++) {
-				if(j==source) dp[i][j] = 0;
-				else {
-					vector<vector<int>> incomingEdges = edges[j];
-					for(int k=0; k<incomingEdges.size(); k++) {
-						int prev = dp[i-1][incomingEdges[k][0]];
-						prev = prev == INT_MAX ? INT_MAX : prev+incomingEdges[k][1];
-						dp[i][j] = min(dp[i][j], prev);
-					}
-				} 
-			}
-		}
-
-		return dp[k][destination];
-	}
+    BellmanFord(int n, int k, vector<vector<int>>& flights) {
+        dp = vector<vector<int>>(k+1, vector<int>(n, INT_MAX));
+        edges = vector<vector<pair<int,int> >>(n);
+        
+        for(int i=0; i<flights.size(); i++) {
+            edges[flights[i][1]].push_back(make_pair(flights[i][0], flights[i][2]));
+        }
+    }
+    
+    int processdp(int n, int l, int source, int dst) {
+        dp[0][source] = 0;
+        
+        for(int i=1; i<=l; i++) {
+            for(int j=0; j<n; j++) {
+                if(j==source) dp[i][j]=0;
+                else {
+                    vector<pair<int,int> > incomingEdges = edges[j];
+                    for(int k=0; k<incomingEdges.size(); k++) {
+                        int prev = dp[i-1][incomingEdges[k].first];
+                        prev = prev==INT_MAX?INT_MAX:prev+incomingEdges[k].second;
+                        dp[i][j] = min(dp[i][j], prev);
+                    }
+                }
+            }
+        }
+        
+        return dp[l][dst];
+    }
 };
 
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        BellmanFord bfa(k+1, n, flights);
+        BellmanFord bf = BellmanFord(n, k+1, flights);
         
-        int minCost = bfa.findMinCost(src,dst,k+1,n);
-
-	    return  minCost == INT_MAX ? -1 : minCost;
+        int minCost = bf.processdp(n, k+1, src, dst);
+        
+        return minCost == INT_MAX?-1:minCost;
     }
 };
