bf455c3d5035c1336addaf887668946320f3edcc
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-09-26 12:07:49 -0400

    Time: 26 ms (5.17%), Space: 11.5 MB (5.24%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,32 +3,26 @@ public:
     vector<vector<int>> res;
     int n;
     
-    vector<int> getVec(vector<int> nums, int j) {
-        vector<int> res;
-        
-        for(int i=0; i<nums.size(); i++) {
-            if(i!=j) res.push_back(nums[i]);
-        }
-        
-        return res;
-    }
-    
-    void bt(vector<int> nums, vector<int> vec) {
+    void bt(vector<int> &nums, vector<int> vec, set<int> s) {
         if(vec.size()==n) {
             res.push_back(vec);
             return;
         }
         
         for(int i=0; i<nums.size(); i++) {
+            if(s.find(i) != s.end()) continue;
             vec.push_back(nums[i]);
-            bt(getVec(nums,i), vec);
+            s.insert(i);
+            bt(nums, vec, s);
+            s.erase(i);
             vec.pop_back();
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         n = nums.size();
-        bt(nums, {});
+        set<int> s;
+        bt(nums, {}, s);
         return res;
     }
 };
