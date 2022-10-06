7d1dc9ba4b77f7c171c34bc98ac66c97b7b5111e
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-10-06 18:29:16 -0400

    Time: 98 ms (13.68%), Space: 12.4 MB (55.58%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
+    vector<pair<int,int>> directions = {
+        make_pair(1,0), make_pair(-1,0),
+        make_pair(0,1), make_pair(0,-1)
+    };
+    
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        for(pair<int,int> direction: directions) {
+            int row = r+direction.first;
+            int col = c+direction.second;
+            
+            if(row<0 || col<0 || row>=grid.size() || col>=grid[0].size() || grid[row][col]=='0')
+                continue;
+            
+            grid[row][col] = '0';
+            dfs(grid, row, col);
+        }
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
-        vector<pair<int,int>> directions = {
-          make_pair(1,0), make_pair(-1,0),
-            make_pair(0,-1), make_pair(0,1)
-        };
-        
-        queue<pair<int,int>> q;
         int res = 0;
         
         for(int i=0; i<grid.size(); i++) {
             for(int j=0; j<grid[0].size(); j++) {
                 if(grid[i][j]=='1') {
-                    q.push(make_pair(i,j));
                     grid[i][j] = '0';
+                    dfs(grid, i, j);
                     res++;
-                }
-                
-                
-                while(!q.empty()) {
-                    int r = q.front().first;
-                    int c = q.front().second;
-                    q.pop();
-                    
-                    for(pair<int,int> direction: directions) {
-                        int row = r+direction.first;
-                        int col = c+direction.second;
-                        
-                        if(row<0 || col <0 || row>=grid.size() || col>=grid[0].size() || grid[row][col]=='0') continue;
-                        grid[row][col] = '0';   
-                        q.push(make_pair(row, col));
-                    }
                 }
             }
         }
