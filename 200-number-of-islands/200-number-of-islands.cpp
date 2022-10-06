151459fe18490def822b19b9df6291449901289e
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-10-06 18:25:22 -0400

    Time: 73 ms (35.60%), Space: 13.2 MB (32.82%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<pair<int,int> > directions = {
-            make_pair(0,1),
-            make_pair(0,-1),
-            make_pair(1,0),
-            make_pair(-1,0)
+        vector<pair<int,int>> directions = {
+          make_pair(1,0), make_pair(-1,0),
+            make_pair(0,-1), make_pair(0,1)
         };
         
-        int cnt = 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int,int> > q;
+        queue<pair<int,int>> q;
+        int res = 0;
         
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
+        for(int i=0; i<grid.size(); i++) {
+            for(int j=0; j<grid[0].size(); j++) {
                 if(grid[i][j]=='1') {
-                    grid[i][j] = '0';
-                    cnt++;
                     q.push(make_pair(i,j));
+                    grid[i][j] = '0';
+                    res++;
                 }
                 
+                
                 while(!q.empty()) {
-                    pair<int,int> curr = q.front();
+                    int r = q.front().first;
+                    int c = q.front().second;
                     q.pop();
                     
-                    for(pair<int,int> direction : directions) {
-                        int r = curr.first + direction.first;
-                        int c = curr.second + direction.second;
+                    for(pair<int,int> direction: directions) {
+                        int row = r+direction.first;
+                        int col = c+direction.second;
                         
-                        if(r<0 || c<0 || r>=m || c>=n || grid[r][c]=='0') continue;
-                        
-                        grid[r][c] = '0';
-                        q.push(make_pair(r,c));
+                        if(row<0 || col <0 || row>=grid.size() || col>=grid[0].size() || grid[row][col]=='0') continue;
+                        grid[row][col] = '0';   
+                        q.push(make_pair(row, col));
                     }
                 }
             }
         }
         
-        return cnt;
+        return res;
     }
 };
