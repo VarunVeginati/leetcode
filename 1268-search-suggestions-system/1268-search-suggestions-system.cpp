18c1fc84a8f80f6a8318ea7e9e2b05c3d3466923
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-10-07 13:30:00 -0400

    Time: 2672 ms (5.02%), Space: 666 MB (5.07%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,72 +1,72 @@
 class Trie{
-private:
-	struct Node {
-		bool isWord = false;
-		vector<Node*> children{vector<Node*>(26, NULL)};
-	} *Root, *curr;
-
-	void dfs(Node* curr, string &word, vector<string> &result) {
-		if(result.size()==3) return;
-
-		if(curr->isWord) result.push_back(word);
-
-		for(char c='a'; c<='z'; c++) {
-			if(curr->children[c-'a'] != NULL) {
-				word += c;
-				dfs(curr->children[c-'a'], word, result);
-				word.pop_back();
-			}
-		}
-	}
-
 public:
-	Trie() {
-		Root = new Node();
-	}
-
-	void insert(string s) {
-		curr = Root;
-
-		for(char c: s) {
-			if(curr->children[c-'a'] == NULL)
-				curr->children[c-'a'] = new Node();
-			curr = curr->children[c-'a'];
-		}
-
-		curr->isWord = true;
-	}
-
-	vector<string> getWordStartingWith(string prefix) {
-		curr = Root;
-		vector<string> result;
-
-		for(char c: prefix) {
-			if(curr->children[c-'a']==NULL)
-				return result;
-			curr = curr->children[c-'a'];
-		}
-		dfs(curr, prefix, result);
-		return result;
-	}
+    struct Node {
+        bool isWord = false;
+        vector<Node*> children = vector<Node*>(26,NULL);
+    };
+    
+    Node* Root;
+    
+    Trie() {
+        Root = new Node();
+    }
+    
+    void addProduct(string s) {
+        Node* curr = Root;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(curr->children[s[i]-'a'] == NULL)
+                curr->children[s[i]-'a'] = new Node();
+                
+            curr = curr->children[s[i]-'a'];
+        }
+        
+        curr->isWord = true;
+    }
+    
+    void dfs(Node* curr, vector<string> &vec, string str) {
+        if(vec.size()>=3 || curr==NULL) return;
+        if(curr->isWord) {
+            vec.push_back(str);
+        }
+        
+        for(int i=0; i<26; i++) {
+            if(curr->children[i] != NULL) {
+                dfs(curr->children[i], vec, str+((char) (i+'a')));
+            }
+        }
+    }
+    
+    vector<string> getSuggestion(string s) {
+        Node* curr = Root;
+        vector<string> vec;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(curr==NULL) return vec;
+            curr = curr->children[s[i]-'a'];
+        }
+        
+        dfs(curr, vec, s);
+        
+        return vec;
+    }
 };
 
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        Trie trie = Trie();
-
-        for(string word: products)
-            trie.insert(word);
+        vector<vector<string>> res;
         
-        vector<vector<string>> result;
-
-        string prefix="";
-
-        for(char c: searchWord) {
-            prefix += c;
-            result.push_back(trie.getWordStartingWith(prefix));
+        Trie trie = Trie();
+        
+        for(int i=0; i<products.size(); i++) {
+            trie.addProduct(products[i]);
         }
         
-        return result;
+        for(int i=0; i<searchWord.size(); i++) {
+            res.push_back(trie.getSuggestion(searchWord.substr(0, i+1)));
+        }
+        
+        return res;
     }
 };
