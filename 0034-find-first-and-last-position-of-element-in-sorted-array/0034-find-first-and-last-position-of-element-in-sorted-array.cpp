7d3bf76227da6454f2e468e4266d58f50a32daad
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-10-26 16:09:24 -0400

    Time: 25 ms (12.03%), Space: 13.7 MB (18.80%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -12,12 +12,34 @@ public:
         return false;
     }
     
+    int lowerBound(vector<int>&nums, int l, int r, int target) {
+        if(l<=r) {
+            int mid = l + ((r-l)/2);
+            
+            if(target <= nums[mid]) return lowerBound(nums, l, mid-1, target);
+            else return lowerBound(nums, mid+1, r, target);
+        }
+        
+        return min(r+1, (int) nums.size()-1);
+    }
+    
+    int upperBound(vector<int>&nums, int l, int r, int target) {
+        if(l<=r) {
+            int mid = l + ((r-l)/2);
+            
+            if(target >= nums[mid]) return upperBound(nums, mid+1, r, target);
+            else return upperBound(nums, l, mid-1, target);
+        }
+        
+        return max(0,l-1);
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         if(!binarySearch(nums, 0, nums.size()-1, target)) return {-1,-1};
         
-        int l = lower_bound(nums.begin(), nums.end(), target)-nums.begin();
-        int u = upper_bound(nums.begin(), nums.end(), target)-nums.begin();
+        int l = lowerBound(nums, 0, nums.size()-1, target);
+        int u = upperBound(nums, 0, nums.size()-1, target);
         
-        return {l, u-1};
+        return {l, u};
     }
 };
