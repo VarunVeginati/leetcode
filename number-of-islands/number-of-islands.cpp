72d9b6e2ae8e5700401b1f4a0df71b513bedb92a
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-11-04 10:44:44 -0400

    Time: 185 ms, Memory: 19.5 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    int m;
-    int n;
-    
-    vector<pair<int,int>> directions = {
-        make_pair(0,1),make_pair(0,-1),
-        make_pair(1,0),make_pair(-1,0)
-    };
-    
-    void dfs(vector<vector<char>> & grid, int i, int j) {
-        for(pair<int,int> direction: directions) {
-            int r = direction.first+i;
-            int c = direction.second+j;
-            
-            if(r<0 || c<0 || r>=m || c>=n || grid[r][c]=='0') continue;
-            
-            grid[r][c] = '0';
-            
-            dfs(grid, r, c);
-        }
-    }
-    
     int numIslands(vector<vector<char>>& grid) {
-        queue<pair<int,int>> q;
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<pair<int,int>> directions = {
+            make_pair(0,1),make_pair(0,-1),
+            make_pair(1,0),make_pair(-1,0)
+        };
+        
         int res = 0;
+        set<pair<int,int>> visited;
+        queue<pair<int,int>> q;
         
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(grid[i][j]=='1') {
-                    res++;
-                    dfs(grid, i, j);
+                if(grid[i][j]=='0' || visited.find(make_pair(i,j)) != visited.end()) continue;
+                
+                res++;
+                q.push(make_pair(i,j));
+                visited.insert(make_pair(i,j));
+                
+                while(!q.empty()) {
+                    int row = q.front().first;
+                    int col = q.front().second;
+                    q.pop();
+                    
+                    for(pair<int,int> direction: directions) {
+                        int r = row + direction.first;
+                        int c = col + direction.second;
+                        
+                        if(r<0 || c<0 || r>=m || c>=n || grid[r][c]=='0' 
+                           || visited.find(make_pair(r,c)) != visited.end()) continue;
+                        
+                        visited.insert(make_pair(r,c));
+                        q.push(make_pair(r,c));
+                    }
                 }
             }
         }
