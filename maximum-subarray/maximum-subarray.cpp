fcb17444322ad543c2ceead0ed51cbb9b641f532
Varun Kumar Veginati <88947725+VarunVeginati@users.noreply.github.com>
2022-11-09 22:26:09 -0500

    Time: 232 ms, Memory: 67.8 MB - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-    vector<int> memo;
-    
-    int dp(int i, vector<int>& nums) {
-        if(i==nums.size()-1) {
-            memo[i] = nums.back();
-            return nums.back();
+    int maxSubArray(vector<int>& nums) {
+        int max_array = nums[0];
+        int max_sum = nums[0];
+        
+        for(int i=1; i<nums.size(); i++) {
+            max_sum = max(nums[i], max_sum+nums[i]);
+            max_array = max(max_array, max_sum);
         }
         
-        if(memo[i] != -1) return memo[i];
-        
-        memo[i] = max(nums[i], nums[i]+dp(i+1, nums));
-        
-        return memo[i];
-    }
-    
-    int maxSubArray(vector<int>& nums) {
-        memo = vector<int>(nums.size(),-1);
-        
-        dp(0, nums);
-        
-        int ans = nums[0];
-        
-        for(int i=0; i<memo.size(); i++)
-            ans = max(ans, memo[i]);
-        
-        return ans;
+        return max_array;
     }
 };
